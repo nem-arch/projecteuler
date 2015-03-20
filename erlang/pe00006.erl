f51857026d8a4a0801c64d11278207c2760556ed
nem <nem@posteo.net>
2015-03-21 00:00:43 +0100

    problem 6 in erlang

---
+++ erlang/pe00006.erl
@@ -0,0 +1,24 @@
+% Sum square difference
+% Problem 6
+% The sum of the squares of the first ten natural numbers is,
+% 
+% 12 + 22 + ... + 102 = 385
+% The square of the sum of the first ten natural numbers is,
+% 
+% (1 + 2 + ... + 10)2 = 552 = 3025
+% Hence the difference between the sum of the squares of the first
+% ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+% 
+% Find the difference between the sum of the squares of the first
+% one hundred natural numbers and the square of the sum.
+-module(pe00006).
+-export([run/0]).
+
+square(N) -> N*N.
+
+sqsum(0,S) -> S;
+sqsum(N,S) ->
+	sqsum(N-1,S+square(N)).
+
+run() ->
+	square(lists:sum(lists:seq(1,100)))-sqsum(100,0).
